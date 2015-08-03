997f734c382ed961a4601555eddb01d12e1195b5
Tyler Langlois <tjl@byu.net>
2015-08-03 00:45:29 -0600

    Solve problem 2

--- src/Euler.hs
+++ src/Euler.hs
@@ -1,5 +1,6 @@
 module Euler
     ( p1
+    , p2
     ) where
 
 import Data.List (union)
@@ -7,3 +8,12 @@ import Data.List (union)
 p1 :: Int -> Int
 p1 x = sum $ union [3,6..top] [5,10..top]
     where top = x - 1
+
+fib :: [Int]
+fib = 0 : 1 : f fib
+    where f (x : l@(y:_)) = (x+y) : f l
+          f (x:xs) = x : f xs
+          f [] = []
+
+p2 :: Int -> Int
+p2 = sum . filter even . flip takeWhile fib . (>)
--- test/EulerSpec.hs
+++ test/EulerSpec.hs
@@ -8,3 +8,7 @@ spec = do
     describe "p1" $ do
         it "correctly calculates the example problem" $ do
             p1 10 `shouldBe` 23
+
+    describe "p2" $ do
+        it "correctly calculates the example problem" $ do
+            p2 100 `shouldBe` 44
